66e13786501601b249d0f7d2735df4d6c92ef37c
jean <jmilsonneau@immersivefactory.com>
2017-07-08 16:48:43 +0200

    Pause Menu

--- Assets/Scripts/FreePlayMode.cs
+++ Assets/Scripts/FreePlayMode.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Utility;
@@ -12,6 +13,10 @@ public class FreePlayMode : MonoBehaviour {
     [SerializeField]
     private int _curCar = 0;
 
+    [SerializeField]
+    private Canvas inGameUI;
+    [SerializeField]
+    private Canvas pauseUI;
     [SerializeField]
     private RectTransform speedo;
 
@@ -27,21 +32,9 @@ public class FreePlayMode : MonoBehaviour {
     }
 
     void Update () {
-
-        if (Input.GetButtonDown("Submit"))
-        {
-            _cars[_curCar].GetComponent<WheelVehicle>().handbreak = true;
-
-            _curCar = (_curCar + 1) % (_cars.Length);
-            Debug.Log(_curCar);
-
-            _camera.GetComponent<SmoothFollow>().Target = _cars[_curCar].transform;
-            _cars[_curCar].GetComponent<WheelVehicle>().handbreak = false;
-        }
-
         if (Input.GetButtonDown("Cancel"))
         {
-            _cars[_curCar].transform.position = new Vector3(0, 4, 0);
+            Pause();
         }
         
         float angle = Mathf.Lerp(speedo.rotation.ToEuler().z, -(_cars[_curCar].GetComponent<WheelVehicle>().speed) / 220 * 180, 0.53f);
@@ -49,5 +42,40 @@ public class FreePlayMode : MonoBehaviour {
         
 
         speedo.rotation = Quaternion.AngleAxis(4 * angle, Vector3.forward);
+    }
+
+    public void SelectCar(Int32 c)
+    {
+        _cars[_curCar].GetComponent<WheelVehicle>().handbreak = true;
+        _cars[_curCar].SetActive(false);
+
+        _curCar = c;
+
+        _camera.GetComponent<SmoothFollow>().Target = _cars[_curCar].transform;
+        _cars[_curCar].GetComponent<WheelVehicle>().handbreak = false;
+        _cars[_curCar].transform.position = Vector3.up;
+        _cars[_curCar].transform.rotation = Quaternion.identity;
+        _cars[_curCar].SetActive(true);
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        
+        pauseUI.gameObject.SetActive(true);
+        inGameUI.gameObject.SetActive(false);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+
+        pauseUI.gameObject.SetActive(false);
+        inGameUI.gameObject.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
