e903fb8ff19819c45bd8306f3b6876ab4bfd32ac
Kwarthys <Kwarthys@gmail.com>
2017-07-10 11:57:00 +0200

    First step for random generation ⚅
    Prefabs well aligning each other to form a first Random track.

---
+++ Assets/Scripts/Gamemodes/RoadGeneration.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadGeneration : MonoBehaviour {
+
+    public GameObject[] prefabs;
+
+    protected Vector3 lastExitPoint = new Vector3(0,0,0);
+    protected Quaternion lastExitPointRot = new Quaternion();
+
+    // Use this for initialization
+    void Start () {
+
+        for(int i = 0; i < 60; i++)
+        {
+            GameObject lastCreated = Instantiate(prefabs[Random.Range(0,255) % prefabs.Length], lastExitPoint, new Quaternion());
+
+            /** Aligning **/
+            Vector3 translation = lastCreated.transform.Find("EntryPoint").transform.position - lastExitPoint;
+            Vector3 rotation = lastExitPointRot.eulerAngles - lastCreated.transform.Find("EntryPoint").eulerAngles;
+
+            if(false)
+            {
+                print("EntryPointPos " + lastCreated.transform.Find("EntryPoint").transform.position);
+                print("ExitPointPos " + lastExitPoint);
+                print("translate " + -translation);
+            }
+
+            if(false)
+            {
+                print("EntryPointPosRot " + lastCreated.transform.Find("EntryPoint").eulerAngles + ". local : " + lastCreated.transform.Find("EntryPoint").localEulerAngles);
+                print("ExitPointPosRot " + lastExitPointRot.eulerAngles);
+                print("Rotate " + rotation);
+            }
+
+            lastCreated.transform.Rotate(rotation);
+            lastCreated.transform.Translate(- translation);
+
+            print("Item " + i + " Placed.");
+            /**************/
+
+
+            lastExitPoint = lastCreated.transform.Find("ExitPoint").transform.position;
+            lastExitPointRot = lastCreated.transform.Find("ExitPoint").transform.rotation;
+        }
+		
+	}
+}
